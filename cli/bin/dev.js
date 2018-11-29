50002a4eda57306ec3af19a8c24b50c71f6a5224
sudm <sudm@suncd.com>
2018-11-29 11:40:32 +0800

    修改脚手架

--- cli/bin/dev.js
+++ cli/bin/dev.js
@@ -1,1 +1,26 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('#!/j/h/g k n 0=7("0"),2=7(\'..\').m,1=\'\';6 9(3){l 3.b(\'@\')}0.a(\'-i, --5 <3>\',\'e d c 2 x\',9).a(\'-f, --4\',\'w v z y q p ...\').o(\'<1>\').r(6(8){1=8}).u(t.s);2({5:0.5,4:0.4,1:1});',36,36,'commander|dirName|theme|name|info|install|function|require|val|versionName|option|split|development|zznode|download||env|bin||usr|node|return|init|let|arguments|templates|presentation|action|argv|process|parse|usage|Download|package|for|commands'.split('|'),0,{}))
+#!/usr/bin/env node
+
+let commander = require("commander"),
+    theme = require('..').init,
+    dirName = '';
+
+function versionName(name) {
+    return name.split('@');
+}
+
+commander
+    .option('-i, --install <name>', 'download zznode development theme package', versionName)
+	.option('-f, --info', 'Download usage commands for presentation templates ...')
+	.option('-v, --version', 'cli version information')
+    .arguments('<dirName>')
+    .action(function(val) {
+        dirName = val;
+    })
+    .parse(process.argv);
+
+theme({
+    install: commander.install,
+	info: commander.info,
+	version: commander.version,
+    dirName: dirName
+});
--- cli/lib/index.js
+++ cli/lib/index.js
@@ -1,1 +1,127 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('p 6=f(\'6\'),n=f(\'n\'),w=f(\'w\'),{1E}=f(\'18\'),17=f(\'1a\'),[k,d]=[0,I];P[19,8,14,13]=[\'4\',\'./16-15\'];P E={1b(a){p 7=a.7,Q=!!a.1h,4=a.R;5(Q){3.2(`z C b 1g B v u:`[\'h\']);3.2(`y-i c@1j<x>`[\'o\']);3.2(`z C b 1i 1d B v u:`[\'h\']);3.2(`y-i c@1c<x>`[\'o\']);3.2(`z C b 1f B v u:`[\'h\']);3.2(`y-i c@1e<x>`[\'o\'])}r 5(!!4&&4.Z>0){5(7===""){7=4[1]}G(7,4)}r{3.2(\'12 Y 11 10 U\')}}}J t G(...a){p[7,4]=a;3.2(`W n F ${4[0]}4 b F ${4[1]}c T N...`);5(!6.H(8))6.X(8);l(\'A\',\'1I ... ...\');p 9=`1H:L n(9,8).O(()=>{}).1K(e=>{3.2(`1J ${4[0]}4 b F ${4[1]}c T 1G U 1D 1C`);m(8)});l(\'1F\');L w(`${8}/${4[1]}.1Q`,7).O(j=>{3.2(\'E N 1L 1P\');m(8);3.2(\'1N 1M 1O 1B 1p: \');3.2(`1o ${7}`);3.2(\'1r R\');3.2(\'1q 1l A\')})}t l(g,D){5(g===\'A\'){g=\'S\';k=0}5(g===\'S\'){d=1k(()=>{k+=V;3.2(`${D}`[\'h\'],`${k/1n}s`[\'o\']);l(g,D)},V)}r{5(d)1m(d);d=I}}J t m(9){K j=[];5(6.H(9)){j=6.1s(9);j.1y(t(M,1x){K q=9+"/"+M;5(6.1A(q).1z()){m(q)}r{6.1u(q)}});6.1t(9)}};1w.1v=E;',62,115,'||log|console|version|if|fs|dirName|TEMP_DIR|path|args|of|theme|timer||require|type|green||files|timeCounter|timerFunc|removeDir|download|red|let|curPath|else||function|Framework|Development|decompress|yourProjectName|zznode|Download|start|System|Command|str|Theme|the|downloadZipFile|existsSync|null|async|var|await|file|dicrionary|then|const|isInfo|install|loading|template|error|500|Begin|mkdirSync|commander|length|is|order|your|NPM_INS|CNPM_INS|temp|dev|color|child_process|LIST_TYPE|colors|init|DDBS|Screen|DEVM|Mobile|Backend|info|Big|BDEV|setTimeout|run|clearTimeout|1000|cd|orders|npm|cnpm|readdirSync|rmdirSync|unlinkSync|exports|module|index|forEach|isDirectory|statSync|these|downloading|when|spawn|close|has|https|Loading|The|catch|downloaded|will|you|exec|success|zip'.split('|'),0,{}))
+/* 主要处理方法 */
+let fs = require('fs'),
+    download = require('download'),
+    decompress = require('decompress'),
+    { spawn } = require('child_process'),
+    color = require('colors'),
+    [timeCounter, timer] = [0, null];
+
+/**
+ * 定义所有静态变量
+ */
+const [LIST_TYPE, TEMP_DIR, CNPM_INS, NPM_INS] = [
+    'version',
+    './dev-temp'
+];
+
+const Theme = {
+    init(args) {
+        /* 重新组装并解析参数 */
+        let dirName = args.dirName,
+			isInfo = !!args.info,
+			isVersion = !!args.version,
+            version = args.install;
+
+        /* 如果是install类型需要下载指定theme和版本，如果版本未指定则设置为master最新版本 */
+		if (isInfo) {
+			console.log(`Download Command of Backend System Development Framework: ` ['green']);
+			console.log(`zznode -i theme@BDEV <yourProjectName>` ['red']);
+			console.log(`Download Command of Big Screen System Development Framework: ` ['green']);
+			console.log(`zznode -i theme@DDBS <yourProjectName>` ['red']);
+			console.log(`Download Command of Mobile System Development Framework: ` ['green']);
+			console.log(`zznode -i theme@DEVM <yourProjectName>` ['red']);
+        } else if (!!version && version.length > 0) {
+            if (dirName === "") {
+                dirName = version[1];
+            }
+            downloadZipFile(dirName, version);
+        } else if (isVersion) {
+			console.log(`now version is 1.1.7`);
+		} else {
+            console.log('your commander order is error');
+        }
+    }
+}
+
+/* 下载制定GitHub zip文件 */
+async function downloadZipFile(...args) {
+    /* 重新定义参数 */
+    let [dirName, version] = args;
+
+    /* 下载提示 */
+    console.log(`Begin download the ${version[0]} version of the ${version[1]} theme template dicrionary ...`);
+
+    /* 如果不存在文件则进行创建文件夹，并下载文件 */
+    if(!fs.existsSync(TEMP_DIR)) fs.mkdirSync(TEMP_DIR);
+
+    timerFunc('start', 'Loading ... ...');
+
+    let path = `https://github.com/demingsu/development-theme/raw/master/${version[0]}/${version[1]}.zip`;
+    await download(path, TEMP_DIR)
+        .then(() => {
+            // console.log(`The ${version[0]} version of the ${version[1]} theme template has downloaded`);
+        }).catch(e => {
+            console.log(`The ${version[0]} version of the ${version[1]} theme template has error when downloading`);
+            
+            /* 删除临时文件 */
+            removeDir(TEMP_DIR);
+        });
+
+    timerFunc('close');
+    /* 根据不同类型解压到不同位置 */
+
+    await decompress(`${TEMP_DIR}/${version[1]}.zip`, dirName)
+        .then(files => {
+            /* 文件下载成功提示 */
+            console.log('Theme dicrionary downloaded success');
+
+            /* 删除临时文件 */
+            removeDir(TEMP_DIR);
+
+            console.log('you will exec these orders: ');
+            console.log(`cd ${dirName}`);
+            console.log('cnpm install');
+            console.log('npm run start');
+        });
+}
+
+/* 超时方法 */
+function timerFunc(type, str) {
+    if (type === 'start') {
+        type = 'loading';
+        timeCounter = 0;
+    }
+    if (type === 'loading') {
+        timer = setTimeout(() => {
+            timeCounter += 500;
+            console.log(`${str}` ['green'], `${timeCounter/1000}s` ['red']);
+            timerFunc(type, str);
+        }, 500);
+    } else {
+        if (timer) clearTimeout(timer);
+        timer = null;
+    }
+}
+
+/* 删除临时目录文件 */
+async function removeDir(path) {
+    var files = [];
+    /* 如果存在文件，读取文件 */
+    if (fs.existsSync(path)) {
+        files = fs.readdirSync(path);
+        /* 遍历所有文件 */
+        files.forEach(function(file, index) {
+            var curPath = path + "/" + file;
+            /* 如果是文件夹，则递归删除，否则直接删除文件 */
+            if (fs.statSync(curPath).isDirectory()) {
+                removeDir(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+        /* 删除主目录 */
+        fs.rmdirSync(path);
+    }
+};
+
+module.exports = Theme;
